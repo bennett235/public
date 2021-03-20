07eaf223405a9c71a9706e0406a575a91b67ad84
bennett235 <76139153+bennett235@users.noreply.github.com>
2021-03-20 16:54:57 -0400

    Add files via upload

---
+++ animation_test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 20:22:37 2020
+
+@author: Bennett
+"""
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+plt.style.use('seaborn-pastel')
+
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 4), ylim=(-2, 2))
+line, = ax.plot([], [], lw=3)
+
+def init():
+    line.set_data([], [])
+    return line,
+def animate(i):
+    x = np.linspace(0, 4, 1000)
+    y = np.cos(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+
+anim = FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=20, blit=True)
+
+
+#anim.save('sine_wave.gif', writer='imagemagick')
+anim.save('sine_wave.gif')
